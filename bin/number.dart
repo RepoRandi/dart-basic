56bf66fee8a5dd8573cc09fc9888681c59c3c734
Randi <randimaulanaakbar@gmail.com>
2022-04-28 17:13:42 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,15 @@
+void main() {
+  int number_1 = 1;
+  double number_2 = 2.5;
+
+  print(number_1);
+  print(number_2);
+
+  num number_3 = 10;
+
+  print(number_3);
+
+  number_3 = 10.5;
+
+  print(number_3);
+}
